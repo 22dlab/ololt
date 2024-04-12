a2c1adb3417b0c31ced27fe9743b56fd1edf69ae
Margad Javkhlan <maagii1130@gmail.com>
2024-04-12 11:50:07 +0800

    feat: update

--- src/components/Navlink.tsx
+++ src/components/Navlink.tsx
@@ -10,10 +10,14 @@ export default function Navlink({
   return (
     <div
       className={`h-full f-ui-1 py-[15px] px-16 box-border bg-transparent hover:bg-secondary transition-colors ${
-        active && !top && 'border-b-2 border-secondary'
+        active && !top && 'border-b-2 border-secondary translate-y-1'
       }`}
     >
-      <p className='text-secondary hover:text-primary transition-colors'>
+      <p
+        className={`text-secondary hover:text-primary transition-colors ${
+          active && !top && '-translate-y-1'
+        }`}
+      >
         {label}
       </p>
     </div>
