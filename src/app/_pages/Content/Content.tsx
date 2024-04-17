090bcd8bc9886dc2c6bf615712f0fab9bcc76c5a
Margad Javkhlan <maagii1130@gmail.com>
2024-04-18 02:34:57 +0800

    feat: update

--- src/app/_pages/Content/Content.tsx
+++ src/app/_pages/Content/Content.tsx
@@ -1,24 +1,30 @@
+'use client'
+
+import { useRef, useEffect, useState } from 'react'
+import { useInView } from 'framer-motion'
+
 import { ContentType } from '@/type'
 
 import TextBlock from './TextBlock'
 import IconCard from './IconCard'
 import ServiceCard from './ServiceCard'
-
 import Photo from './Photo'
 
 export default function Content({ content }: { content: ContentType[] }) {
+  const refs = useRef<Array<HTMLDivElement | null>>([])
+
   return (
     <div className='container py-96'>
-      <div className='w-full flex'>
-        <div className='w-3-cols-vw mr-gutter'>
+      <div className='w-full flex relative h-auto'>
+        <div className='w-3-cols-vw mr-gutter sticky top-96 self-start'>
           <p className='f-heading-5'>Specialized Areas</p>
           <div className='mt-32'>
             {content.slice(1, content.length).map((item, index) => (
               <div
                 key={index}
-                className='px-16 py-12 f-ui-1 border-l-4 box-border border-secondary'
+                className={`px-16 py-12 f-ui-1 border-l-4 box-border border-primary`}
               >
-                <a href='#'>{item.name.en}</a>
+                <a href={`#${item.name.en.toLowerCase()}`}>{item.name.en}</a>
               </div>
             ))}
           </div>
@@ -26,7 +32,14 @@ export default function Content({ content }: { content: ContentType[] }) {
         <div className='w-9-cols-vw space-y-48'>
           {content.map((x, index) => {
             return (
-              <div key={index} className='space-y-60'>
+              <div
+                key={index}
+                id={x.name.en.toLowerCase()}
+                ref={(element) => {
+                  refs.current.push(element)
+                }}
+                className='space-y-60'
+              >
                 {index !== 0 && (
                   <div className='f-heading-1 pt-48 border-t border-primary w-6-cols-vw'>
                     <p>{x.name.en}</p>
--- src/app/_pages/Content/TextBlock.tsx
+++ src/app/_pages/Content/TextBlock.tsx
@@ -1,11 +1,22 @@
 import { ItemType } from '@/type'
 
-import { PortableText } from 'next-sanity'
+import { PortableText, PortableTextComponents } from 'next-sanity'
 
 export default function TextBlock({ item }: { item: ItemType }) {
+  const components: PortableTextComponents = {
+    block: {
+      h4: ({ children }) => <h4 className='f-heading-4'>{children}</h4>
+    },
+    list: {
+      bullet: ({ children }) => (
+        <ul className='list-disc list-outside ml-24'>{children}</ul>
+      )
+    }
+  }
+
   return (
     <div className='w-6-cols-vw f-body-1 space-y-16'>
-      <PortableText value={item.en} />
+      <PortableText value={item.en} components={components} />
     </div>
   )
 }
