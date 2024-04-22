0010aae527d9b11d1a9caf665801ca500888d9ef
Margad Javkhlan <maagii1130@gmail.com>
2024-04-23 00:24:40 +0800

    feat: update

--- src/app/_pages/Home/Partners.tsx
+++ src/app/_pages/Home/Partners.tsx
@@ -6,7 +6,7 @@ export default function Partners() {
       <p className='text-center f-heading-4 text-secondary'>
         International partners
       </p>
-      <div className='container grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 px-48 py-20 bg-primary mt-40 gap-y-24 gap-x-16'>
+      <div className='container grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 md:px-48 py-20 bg-primary mt-40 gap-y-24 gap-x-16'>
         {[...Array(9)].map((item, index) => (
           <div key={index} className='w-full h-[56px] relative'>
             <Image
