546de115e80ae1b444c2e5fd9bc3df5dff1d89f5
Margad Javkhlan <maagii1130@gmail.com>
2024-04-25 11:00:19 +0800

    feat: update

--- src/app/_pages/About/About.tsx
+++ src/app/_pages/About/About.tsx
@@ -42,56 +42,67 @@ export default function About() {
     {
       icon: '/icon/pioneer.svg',
       title: {
-        en: 'PIONEERING',
-        mn: ''
+        en: 'INTEGRITY & TRANSPARENCY',
+        mn: 'ЁС ЗҮЙ БА ИЛ ТОД БАЙДАЛ'
       },
       text: {
-        en: 'Acknowledging the needs for development of market mechanisms that ensures diverse stakeholders engagement to address climate change for long lasting impact and effectiveness, We are dedicated to initiate and develop the establishment of new market mechanisms at the national level.',
-        mn: ''
+        en: 'We uphold: At our core, we uphold ethical conduct and transparency ensuring trust and accountability to our stakeholders. To foster a culture of integrity and responsibility, we also advocate for transparency within organizations, encouraging them to assess their business impact on society and the environment.',
+        mn: 'Бид ёс зүй, ил тод байдал, итгэлцэлд түшиглэн хариуцлагатай хамтран ажиллах замаар байгууллагуудын нийгэм, байгаль орчинд үзүүлэх нөлөөллийг бодитоор үнэлж, нээлттэй ил тод байх зарчмыг эрхэмлэнэ.'
       }
     },
     {
       icon: '/icon/domain.svg',
       title: {
-        en: 'DOMAIN EXPERTISE AND ACUMEN',
-        mn: ''
+        en: 'INNOVATION & PIONEERING',
+        mn: 'ШИЙДЭЛ БА МАНЛАЙЛАЛ'
       },
       text: {
-        en: 'Our team excels in the field of climate change boasting extensive proficiency in areas such as climate change policy, GHG accounting, and carbon finance. With a proven track record in crafting and executing national-level projects and research initiatives aligned with international standards and requirements, our team members are widely respected for their substantial contributions in these fields.',
-        mn: ''
+        en: 'We are committed: Recognizing the imperative for a solution-based approach to engage diverse stakeholders in tackling climate change with lasting impact and effectiveness, we are committed to pioneering innovation. Our dedication lies in initiating and cultivating the establishment of new country-specific mechanisms, driving progress towards a sustainable future.',
+        mn: 'Уур амьсгалын өөрчлөлтийг үр дүнтэй сааруулж, олон талын эрх ашиг, оролцоог хангахад шийдэлд түшиглэсэн арга хэмжээ чухал бөгөөд үүнд шинэлэг, анхдагч, инновацлаг байх зарчмыг бид эрхэмлэнэ.'
       }
     },
     {
       icon: '/icon/adherence.svg',
       title: {
-        en: 'ADHERENCE TO GLOBAL STANDARDS',
-        mn: ''
+        en: 'RESEARCH & SCIENCE-BASED APPROACH',
+        mn: 'ШИНЖЛЭХ УХААНЧ АРГА БАРИЛ'
       },
       text: {
-        en: 'Acknowledging the  importance of adherence to international standards and methodologies, We strive to strengthen our services to meet highest quality by obtaining accreditation from widely recognized standards and combining scientific findings and in-depth research.',
-        mn: ''
+        en: 'We believe: We firmly believe in adopting a research and science-based approach in all our endeavors. By grounding our actions and decisions in empirical evidence and rigorous analysis, we ensure that our initiatives are well-informed and effective. This commitment to scientific integrity guides us in addressing complex challenges such as climate change, enabling us to develop sustainable solutions that stand the test of time.',
+        mn: 'Бид үйл ажиллагааныхаа чанар, үр дүн, нөлөөллийг баталгаажуулахдаа судалгаа шинжилгээ, шинжлэх ухаанд суурилсан хандлагыг баримтлах ба энэ нь урт хугацааны сорилтыг давсан үр дүнд хүрэх суурь болно.'
       }
     },
     {
       icon: '/icon/diverse.svg',
       title: {
-        en: 'DIVERSE, INCLUSIVE COLLABORATION',
-        mn: ''
+        en: 'CULTURAL VALUE & CONTEXT SPECIFIC',
+        mn: 'Үндэсний үнэ цэнэ, онцлогт нийцсэн'
       },
       text: {
-        en: 'We recognize that addressing climate change necessitates involvement from all sectors of society, including governments, businesses, and individuals. Engaging stakeholders and fostering robust partnerships are crucial for the effective implementation of policies and actions in the long run. Renowned for our adeptness in fostering fruitful collaborations, we prioritize inclusive and high-integrity partnerships with a diverse spectrum of stakeholders in all our core activities.',
-        mn: ''
+        en: 'We prioritize the principle of cultural value and context-specific approaches in all our endeavors. Recognizing the unique needs of our stakeholders, we tailor our strategies and solutions to honor their requirements and considerations of national values. By embracing cultural values and sensitivity, we ensure that our initiatives resonate authentically with the communities we serve, fostering meaningful and sustainable impact.',
+        mn: 'Түнш талын хэрэгцээ, онцлогт нийцсэн үйлчилгээ үзүүлэхдээ үндэсний үнэ цэнийг эрхэмлэх зарчмыг баримтална. Энэ нь үндэсний ашиг сонирхолд нийцсэн урт хугацаанд тогтвортой, үр дүнтэй үйл ажиллагаа явуулах суурь болно.'
       }
     },
     {
       icon: '/icon/ethic.svg',
       title: {
-        en: 'ETHICAL & TRANSPARENT',
-        mn: ''
+        en: 'ADHERENCE TO GLOBAL STANDARDS',
+        mn: 'ОЛОН УЛСЫН СТАНДАРТАД НИЙЦСЭН'
       },
       text: {
-        en: 'At our core, we uphold ethical conduct and transparency ensuring trust and accountability to our stakeholders. To foster a culture of integrity and responsibility, we also advocate for transparency within organizations, encouraging them to assess their business impact on society and the environment.',
-        mn: ''
+        en: 'Recognizing the paramount importance of adhering to international standards and methodologies, we are committed to enhancing the quality of our services. We aim to achieve this by obtaining accreditation from widely recognized standards and integrating scientific findings and in-depth research into our practices. This steadfast commitment ensures that we meet the highest standards of excellence and fulfill our obligations to our stakeholders and partners.',
+        mn: 'Бид үйл ажиллагааныхаа чанарыг сайжруулах зорилгоор олон улсад хүлээн зөвшөөрөгдсөн шаардлага, стандарттай нийцүүлэх зарчмыг баримталдаг.'
+      }
+    },
+    {
+      icon: '/icon/ghg.svg',
+      title: {
+        en: 'DIVERSE, INCLUSIVE COLLABORATION',
+        mn: 'ОЛОН ТАЛТ ХАМТЫН АЖИЛЛАГАА'
+      },
+      text: {
+        en: 'We recognize that addressing climate change necessitates involvement from all sectors of society, including governments, businesses, and individuals. Engaging stakeholders and fostering robust partnerships are crucial for the effective implementation of policies and actions in the long run. Renowned for our adeptness in fostering fruitful collaborations, we prioritize inclusive and high-integrity partnerships with a diverse spectrum of stakeholders in all our core activities.',
+        mn: 'Уур амьсгалын өөрчлөлтийн цогц асуудлыг шийдвэрлэхэд бүхий л талын оролцоо, хамтын ажиллагаа амин чухал болохыг ухамсарлаж, бүхий л үйл ажиллагаандаа олон талуудын оролцоог хангаж, шудрага, тэгш эрхтэй түншлэлийг эрхэмлэнэ.'
       }
     }
   ]
@@ -105,10 +116,12 @@ export default function About() {
           </div>
           <div className='md:w-6-cols-vw'>
             <p className='f-body-1'>
-              We are aspired to contribute to the pursuit of a climate smart and
+              Our vision is to contribute to the pursuit of a climate-smart and
               sustainable society by strategically employing a combination of
               innovative market and non-market instruments firmly grounded in
-              integrity.
+              integrity. We aspire to lead the way towards a future where
+              environmental stewardship and social responsibility are paramount,
+              creating a world that thrives on sustainability and resilience.
             </p>
           </div>
         </div>
@@ -118,11 +131,12 @@ export default function About() {
           </div>
           <div className='md:w-6-cols-vw'>
             <p className='f-body-1'>
-              Our mission is to efficiently scale up our endeavors, to push
-              forward our national targets in alignment with the Paris Agreement
-              through prioritizing strategic partnerships, empowering
-              professional communities, fostering scientific and economic
-              research, and advocating for change.
+              Our mission is to effectively scale our efforts to advance
+              national targets in line with the Paris Agreement. Through
+              strategic partnerships, empowering professional communities,
+              conducting scientific and economic research, and advocating for
+              change, we aim to foster sustainable change while ensuring
+              accountability, equity, and integrity in all our endeavors.
             </p>
           </div>
         </div>
@@ -132,24 +146,57 @@ export default function About() {
           </div>
           <div className='md:w-6-cols-vw'>
             <p className='f-body-1'>
-              The OLOLT Center was founded with a strong commitment to
-              bolstering Mongolia&apos;s efforts in mitigating and adapting to
-              adverse impacts of climate change.
+              In the vast and dynamic landscape of Mongolia&pos;s environmental
+              advocacy, OLOLT Center distinguishes itself as a beacon of
+              innovation and impact. Born from a collective passion for
+              combating climate change and promoting sustainable development,
+              the OLOLT Center has emerged as a significant contributor in
+              Mongolia&pos;s climate change sector.
               <br />
               <br />
-              While our country&apos;s share of global greenhouse gas emissions
-              is relatively small, our susceptibility to climate-related effects
-              is substantial, especially given our dependence on sectors like
-              mining and agriculture, which are particularly vulnerable to
-              climate impacts.
+              At the heart of OLOLT Center&pos;s mission is a commitment to
+              interdisciplinary collaboration, recognizing that addressing
+              climate change requires a holistic approach that spans across
+              various sectors. From environment and energy to agriculture and
+              ESG initiatives, OLOLT Center&pos;s agenda knows no bounds.
               <br />
               <br />
-              Taking into account our national context, as well as the awareness
-              and capabilities of stakeholders, and drawing from our
-              experts&apos; diverse interdisciplinary expertise, we are
-              dedicated to fostering sustainable and impactful climate action.
-              This involves deploying both market and non-market mechanisms to
-              engage various stakeholders effectively.
+              What sets OLOLT Center apart is not just its ambitious goals, but
+              the expertise and dedication of its team, which includes
+              well-respected national experts alongside valued international
+              partners. Together, they bring a wealth of knowledge and
+              experience to the table, making OLOLT Center a force to be
+              reckoned with.
+              <br />
+              <br />
+              In a series of groundbreaking achievements, OLOLT Center is aiming
+              to establish itself as a key player in Mongolia&pos;s
+              environmental landscape. As the first Mongolian member
+              organization to join the Global Reporting Initiative (GRI), OLOLT
+              Center is aiming to set a precedent for transparency and
+              accountability.
+              <br />
+              <br />
+              But our journey doesn&pos;t stop there. With a pioneering spirit,
+              OLOLT Center became the Local Issuer for the I-Track Foundation,
+              marking a significant milestone in the nation&pos;s quest for
+              promoting renewable energy production.
+              <br />
+              <br />
+              Perhaps most notably, OLOLT Center has taken on the mantle of
+              being the first professional organization dedicated to carbon
+              market development at the national level, making it a pioneer in
+              developing the domestic voluntary carbon market, particularly in
+              the forest sector. With a firm belief in the transformative
+              potential of carbon markets to catalyze sustainable change, OLOLT
+              Center is at the forefront of shaping a greener, more resilient
+              future for Mongolia.
+              <br />
+              <br />
+              As we continue to write our story, one thing remains clear: our
+              passion, expertise, and relentless pursuit of environmental
+              excellence will continue to inspire change and leave a lasting
+              impact on Mongolia and beyond.
             </p>
           </div>
         </div>
